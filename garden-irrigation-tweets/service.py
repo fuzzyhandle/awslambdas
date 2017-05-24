92c6a0ec54e036daf02e73c5b4fa76727480b807
fuzzyhandle <hrishikesh.date@gmail.com>
2017-05-24 16:43:14 +0530

    AWS Lambda function to tweet based on updated to a Thing's shadow

---
+++ garden-irrigation-tweets/service.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+import tweepy
+import json
+import pprint
+
+
+# Using twitter account hd_irrigation01
+# Using keys from app https://apps.twitter.com/app/13837356/keys
+# Email for account hrishikesh.date+hd_irrigation01@gmail.com
+
+with open('./twitterconfig.json', 'r') as file:
+    config = json.loads(file.read())
+    
+    CONSUMER_KEY = config['consumer-key']
+    CONSUMER_SECRET = config['consumer-secret']
+    ACCESS_TOKEN = config['access-token']
+    ACCESS_TOKEN_SECRET = config['access-token-secret']
+
+
+def handler(event, context):
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(event)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+    
+    stateobj = None
+    statereportedobj = None
+    statedesiredobj = None
+    
+    metadataobj = None
+    
+    if event.has_key("state"):
+      stateobj = event["state"]
+      statedesiredobj = stateobj.get("desired")
+      statereportedobj = stateobj.get("reported")
+      
+    if event.has_key("metadata"):
+      metadataobj = event["metadata"
+    
+    if statereportedobj:
+      #Check if this is a heatbeat update
+      if metadataobj.get("reported",{}).has_key("heartbeat"):
+        heartbeat = statereportedobj["heartbeat"]
+        api.update_status(u'Received Heartbeat with value {0}%'.format(heartbeat)
+
+      if metadataobj.get("reported",{}).has_key("lastwaterting"):
+        lastwaterting = statereportedobj["lastwaterting"]
+        api.update_status(u'Irrigation cycle ran at {0}%'.format(lastwaterting)
+      
+    if statedesiredobj:
+      if metadataobj.get("desired",{}).has_key("waternow"):
+        waternow = statedesiredobj["waternow"]
+        if waternow:
+          api.update_status(u'Its time to run the irrigation cycle')
