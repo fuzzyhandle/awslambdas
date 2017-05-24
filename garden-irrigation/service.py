65b71077083229867710d0961ade5f21e6154d5b
fuzzyhandle <hrishikesh.date@gmail.com>
2017-05-24 10:24:44 +0530

    Make core more readable. Add a fuzzy logic to handle edge cases where lastwatering time is incorrectly set to uptime by The thing when ntp time update takes longer than usual

--- garden-irrigation/service.py
+++ garden-irrigation/service.py
@@ -10,14 +10,17 @@ TIMEZONEOFFSET = 5 * 3600 + 30 * 60
 THINGNAME = 'esp8266_D42709'
 DEFAULT_SLEEP_INTERVAL = 60*60
 
+DESIRED_KEY = "desired"
+REPORTED_KEY = "reported"
+
 def redundant_water_off (desiredstate):
     desiredstate["waternow"] = False
     print ("We need to ensure pump is OFF")
-    push_desired_state (desiredstate)
+    push_state (DESIRED_KEY, desiredstate)
     
-def push_desired_state (desiredstate):
+def push_state (statekey, state):
     mypayload = json.dumps(
-      {"state":{"desired":desiredstate}}
+      {"state":{statekey:state}}
     )
     client = boto3.client('iot-data')
     response = client.update_thing_shadow(thingName= THINGNAME,payload=mypayload)
@@ -25,13 +28,16 @@ def push_desired_state (desiredstate):
 def dologic(stateobj):
     
     nowepoch = int(time.time())
+    nowepochinlocaltime = nowepoch +TIMEZONEOFFSET
 
-    secondsfrommidnight = (nowepoch +TIMEZONEOFFSET) % 86400
+    secondsfrommidnight = (nowepochinlocaltime) % 86400
     print ("Seconds from midnight is {0}".format(secondsfrommidnight))
     
     desiredstate =  stateobj['state']['desired']
     reportedstate =  stateobj['state']['reported']
-    
+    metadata =  stateobj['metadata']
+    print (metadata)
+
     starttimefrommidnight = desiredstate.get ("dailystarttime",0)
     endtimefrommidnight = desiredstate.get ("dailyendtime",0)
     desiredstate["sleepinterval"] = DEFAULT_SLEEP_INTERVAL
@@ -53,21 +59,32 @@ def dologic(stateobj):
 
     lastwatering = reportedstate.get ("lastwatering",0)
     dosageinterval = desiredstate.get ("dosageinterval",10800)
-    
     print ("Last Watering on {0}. Dosage Interval is {1}. Current Time is {2}".format(lastwatering,dosageinterval,nowepoch))
     
+    lastwatering = reportedstate.get ("lastwatering",0)
+    lastwateringupdate = metadata[REPORTED_KEY].get("lastwatering").get("timestamp",nowepochinlocaltime)
+
+    #Less than 1st Jan 2017
+    if lastwatering < 1483209000:
+      #Something is wrong. use fuzzy logic
+      #Update the last watering time to current time to avoid rewatering in the next wake up of The thing
+      print ("Something is wrong. Last Watering time is {0}. Last Watering time updated on {1}. Fuzzing it up".format(lastwatering,lastwateringupdate))
+      push_state(REPORTED_KEY,{"lastwatering":lastwateringupdate})
+      #Dont do any further processing. Next invocation will hopefully won't come here
+      return
+
     if nowepoch >= lastwatering + dosageinterval:
       #Its time for action
       print ("We need to start the pump")
       desiredstate["waternow"] = True
-      push_desired_state(desiredstate)
+      push_state(DESIRED_KEY,desiredstate)
       client = boto3.client('iot-data')
       print (stateobj)
       response = client.update_thing_shadow(thingName= THINGNAME,payload=mypayload)
     else:
       deltatonextwatering = ( lastwatering + dosageinterval) - nowepoch
       desiredstate["sleepinterval"] = deltatonextwatering
-      push_desired_state(desiredstate)
+      push_state(DESIRED_KEY,desiredstate)
       
       
 def handler(event, context):
